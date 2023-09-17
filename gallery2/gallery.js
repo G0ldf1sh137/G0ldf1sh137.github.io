ca5a40394ad191788b60f92ed4fd2c6279d03b28
G0ldf1sh137 <m.rich137@gmail.com>
2023-09-16 19:09:03 -0500

    cleanup swiper gallery

--- gallery2/gallery.js
+++ gallery2/gallery.js
@@ -2,6 +2,9 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  speed: 400,
+  spaceBetween: 100,
+  setwrapperSize: true,
 
   // If we need pagination
   pagination: {
